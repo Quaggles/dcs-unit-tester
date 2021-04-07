5c373be725b3082dc34ab39c8d4986cc4bfe5956
Quaggles <RyanQuagliata@gmail.com>
2021-04-07 19:51:25 +1000

    OnMissionEnd cleaner output

--- MissionScripts/OnMissionEnd.lua
+++ MissionScripts/OnMissionEnd.lua
@@ -1,12 +1,18 @@
 -- If there are any undamaged units on red team
+local failed = false
 for i, group in pairs(coalition.getGroups(1)) do
     for i, unit in pairs(group:getUnits()) do
         local life = unit:getLife()
         local life0 = unit:getLife0()
         Output("G: "..group:getName().." U: "..unit:getName()..' - life: '..life.."/"..life0)
         if life >= life0 then
-            Assert(false);
+			if not failed then
+				Assert(false);
+			end
+			failed = true
         end
     end
 end
-Assert(true);
+if not failed then
+	Assert(true);
+end

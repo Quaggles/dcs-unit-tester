7e62034ff38e8bacf27e4ea8679852d034406036
Quaggles <8382945+Quaggles@users.noreply.github.com>
2024-04-03 22:37:54 +1000

    Updated OnMissionEnd to fail if any red statics are alive

--- MissionScripts/OnMissionEnd.lua
+++ MissionScripts/OnMissionEnd.lua
@@ -6,13 +6,17 @@ for i, group in pairs(coalition.getGroups(1)) do
         local life0 = unit:getLife0()
         Output("G: "..group:getName().." U: "..unit:getName()..' - life: '..life.."/"..life0)
         if life >= life0 then
-			if failed == false then
-				Assert(false);
-			end
 			failed = true
         end
     end
 end
-if failed == false then
-	Assert(true);
+-- If there are any undamaged statics on red team
+for i, static in pairs(coalition.getStaticObjects(1)) do
+    local life = static:getLife()
+    local life0 = static:getDesc().life
+    Output("S: "..static:getName()..' - life: '..life.."/"..life0)
+    if life >= life0 then
+        failed = true
+    end
 end
+Assert(failed == false);
